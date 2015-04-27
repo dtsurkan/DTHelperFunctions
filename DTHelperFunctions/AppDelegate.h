1134a0a79c497d207991938bc0e2edef9dae4f13
Dmitriy Tsurkan <dmitriy.tsurkan@bizico.com>
2015-04-27 14:02:11 +0300

    Initial Commit

---
+++ DTHelperFunctions/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DTHelperFunctions
+//
+//  Created by DmitriyTsurkan on 4/27/15.
+//  Copyright (c) 2015 dtsurkan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
